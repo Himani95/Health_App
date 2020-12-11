9d7b2e629e22be0c3a661540c34343c701f326d5
Himani Mense <himanimense@Himanis-MacBook-Pro.local>
2020-12-11 11:12:24 -0800

    Updates

---
+++ precache-manifest.4d2c944aac0389bbe0e98254d914437f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eabbe03cdb33bec79930245078b343a",
+    "url": "/Health_App/index.html"
+  },
+  {
+    "revision": "eee4e8612c1bf22e8899",
+    "url": "/Health_App/static/css/main.045fdf19.chunk.css"
+  },
+  {
+    "revision": "aeadc898a427e98d0651",
+    "url": "/Health_App/static/js/2.8ca04ec9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Health_App/static/js/2.8ca04ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee4e8612c1bf22e8899",
+    "url": "/Health_App/static/js/main.ffaa9d12.chunk.js"
+  },
+  {
+    "revision": "752f848fac985f2005e1",
+    "url": "/Health_App/static/js/runtime-main.1c62e9d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Health_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
